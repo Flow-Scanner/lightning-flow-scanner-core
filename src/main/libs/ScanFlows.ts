9b6ff3b47aca78244045b8a22f09cb9bf6aa6866
Ruben Halman <halmanrf@gmail.com>
2025-10-15 01:06:03 +0200

    rename scaninternal to scan2

--- src/main/libs/ScanFlows.ts
+++ src/main/libs/ScanFlows.ts
@@ -7,20 +7,18 @@ import {
   RuleResult,
   ScanResult,
 } from "../../main/internals/internals";
-import { AdvancedRuleConfig } from "../interfaces/AdvancedRuleConfig";
-import { AdvancedRule } from "../models/AdvancedRule";
 import { ParsedFlow } from "../models/ParsedFlow";
-import { BetaRuleStore, DefaultRuleStore } from "../store/DefaultRuleStore";
-import { DynamicRule } from "./DynamicRule";
 import { GetRuleDefinitions } from "./GetRuleDefinitions";
+import { scan2 } from "./Scan";
 
-const { IS_NEW_SCAN_ENABLED: isNewScanEnabled, OVERRIDE_CONFIG: overrideConfig } = process.env;
+const { IS_NEW_SCAN_ENABLED: isNewScanEnabled } = process.env;
 
-// Will be replaced by scanInternal in the future
 export function scan(parsedFlows: ParsedFlow[], ruleOptions?: IRulesConfig): ScanResult[] {
   if (isNewScanEnabled === "true") {
-    return scanInternal(parsedFlows, ruleOptions);
+    return scan2(parsedFlows, ruleOptions);
   }
+
+  // TD Will be removed once scan2 is fully enabled
   const flows: Flow[] = [];
   for (const flow of parsedFlows) {
     if (!flow.errorMessage && flow.flow) {
@@ -39,7 +37,6 @@ export function scan(parsedFlows: ParsedFlow[], ruleOptions?: IRulesConfig): Sca
   return scanResults;
 }
 
-// Will be removed once scanInternal is fully enabled
 // eslint-disable-next-line sonarjs/cognitive-complexity
 export function ScanFlows(flows: Flow[], ruleOptions?: IRulesConfig): ScanResult[] {
   const flowResults: ScanResult[] = [];
@@ -86,15 +83,6 @@ export function ScanFlows(flows: Flow[], ruleOptions?: IRulesConfig): ScanResult
   return flowResults;
 }
 
-export function scanInternal(parsedFlows: ParsedFlow[], ruleOptions?: IRulesConfig): ScanResult[] {
-  const flows: Flow[] = parsedFlows.map((parsedFlow) => parsedFlow.flow as Flow);
-  const scanResults: ScanResult[] = [];
-  for (const flow of flows) {
-    scanResults.push(scanFlowWithConfig(flow, ruleOptions));
-  }
-  return scanResults;
-}
-
 function generalSuppressions(scanResults: ScanResult[], ruleOptions?: IRulesConfig) {
   if (!ruleOptions?.exceptions) {
     return;
@@ -124,63 +112,7 @@ function generalSuppressions(scanResults: ScanResult[], ruleOptions?: IRulesConf
   }
 }
 
-function ruleAndConfig(
-  ruleOptions?: IRulesConfig
-): [Record<string, AdvancedRule>, AdvancedRuleConfig] {
-  // for unit tests, use a small set of rules
-  const ruleConfiguration = unifiedRuleConfig(ruleOptions);
-  let allRules: Record<string, AdvancedRule> = { ...DefaultRuleStore, ...BetaRuleStore };
-  if (
-    overrideConfig === "true" &&
-    ruleOptions?.rules &&
-    Object.keys(ruleOptions.rules).length > 0
-  ) {
-    allRules = Object.entries(allRules).reduce<Record<string, AdvancedRule>>(
-      (accumulator, [ruleName, rule]) => {
-        if (ruleOptions?.rules?.[ruleName]) {
-          accumulator[ruleName] = rule;
-        }
-        return accumulator;
-      },
-      {}
-    );
-  }
 
-  return [allRules, ruleConfiguration];
-}
 
-function scanFlowWithConfig(flow: Flow, ruleOptions?: IRulesConfig): ScanResult {
-  const [allRules, ruleConfiguration] = ruleAndConfig(ruleOptions);
-  const ruleResults: RuleResult[] = [];
-  for (const [ruleName] of Object.entries(allRules)) {
-    const rule = new DynamicRule<AdvancedRule>(ruleName) as AdvancedRule;
-    if (
-      !rule.supportedTypes.includes(flow.type) ||
-      ruleConfiguration?.[ruleName]?.disabled === true
-    ) {
-      ruleResults.push(new RuleResult(rule as IRuleDefinition, []));
-      continue;
-    }
 
-    if (ruleConfiguration?.[ruleName]?.severity) {
-      rule.severity = ruleConfiguration[ruleName].severity as string;
-    }
-    const flowName = flow.name as string;
-    const userRuleConfiguration = ruleConfiguration[ruleName] ?? {};
-    const userFlowSuppressions: string[] = ruleOptions?.exceptions?.[flowName]?.[ruleName] ?? [];
 
-    ruleResults.push(rule.execute2(flow, userRuleConfiguration, userFlowSuppressions));
-  }
-  return new ScanResult(flow, ruleResults);
-}
-
-function unifiedRuleConfig(ruleOptions: IRulesConfig | undefined): AdvancedRuleConfig {
-  const configuredRules: AdvancedRuleConfig = ruleOptions?.rules ?? {};
-  const activeConfiguredRules: AdvancedRuleConfig = Object.entries(
-    configuredRules
-  ).reduce<AdvancedRuleConfig>((accumulator, [ruleName, config]) => {
-    return { ...accumulator, [ruleName]: config };
-  }, {});
-
-  return activeConfiguredRules;
-}
