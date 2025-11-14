d5f719a146689e47e40a2785535733f9fce30c09
smartinello1 <martinellosimone01@gmail.com>
2025-11-15 00:56:32 +0100

    feat: Add New Rule - MissingMetadataDescription (for Elements & Variables) #215
    New Rule Implementation - MissingMetadataDescription
    
    Checks for every flow's element if has a property description filled

---
+++ src/main/rules/MissingMetadataDescription.ts
@@ -0,0 +1,33 @@
+import * as core from "../internals/internals";
+import { RuleCommon } from "../models/RuleCommon";
+import { IRuleDefinition } from "../interfaces/IRuleDefinition";
+
+export class MissingMetadataDescription extends RuleCommon implements IRuleDefinition {
+  constructor() {
+    super({
+      name: "MissingMetadataDescription",
+      label: "Missing Metadata Description",
+      description: "Every element must have a meaningful description",
+      supportedTypes: core.FlowType.allTypes(),
+      docRefs: [],
+      isConfigurable: false,
+      autoFixable: false,
+    });
+  }
+
+  public execute(flow: core.Flow, options?: object, suppressions: string[] = []): core.RuleResult {
+    return this.executeWithSuppression(flow, options, suppressions, (suppSet) => {
+      const results: core.ResultDetails[] = flow.elements
+        .filter((elem) => {
+          if (!elem.element["description"]) {
+            return elem;
+          }
+        })
+        .map((elem) => {
+          return new core.ResultDetails(elem);
+        });
+
+      return new core.RuleResult(this, results);
+    });
+  }
+}
